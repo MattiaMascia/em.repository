478795d19fad7807b1781baf7e038b00348e7070
MattiaMascia <mattmas00@gmail.com>
2024-09-26 14:58:15 +0200

    Add files via upload

---
+++ 08-27/Prova.py
@@ -0,0 +1,10 @@
+nome = "Alice"
+età = 25
+print("Il mio nome è", nome,"e ho", età, "anni")
+
+nome = input("Inserisci il tuo nome: ")
+print("Ciao, " + nome + "! Benvenuto in Python")
+
+print(1+2)
+print(6-5)
+print(5%2)
